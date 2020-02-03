801fa8005733a6f3e09fbb6176da73facbb58ca3
candescent-mouse-0917 <aqiph818@gmail.com>
2020-02-03 01:15:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ function divide(a, b){
   return a/b
 }
 
-//n1 = 10;
-var n2 = 10;
+n1 = 10;
+//var n2 = 10;
 function add5(){
-//  n1 += 5;
-  n2 += 5;
+  n1 += 5;
+//  n2 += 5;
 }
 function add5()
-//console.log(n1);
-console.log(n2);
+console.log(n1);
+//console.log(n2);
 
